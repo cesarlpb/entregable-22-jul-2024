cb460260a47f8a174a5e44a30dd363ddc49394f2
César <cesarlpb@gmail.com>
2024-07-22 15:34:14 +0200

    script para probar proyecto

---
+++ startup.sh
@@ -0,0 +1,10 @@
+  #!/bin/bash
+
+  # Cambiar solo carpeta proyecto
+  cd <carpeta_de_proyecto>
+  
+  # NO CAMBIAR:
+  python -m venv .venv && source .venv/bin/activate
+  pip install -r requirements.txt
+  python manage.py makemigrations && python manage migrate
+  python manage.py runserver
