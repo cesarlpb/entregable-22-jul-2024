c428bbf0a9f2c408a9b2c0353aee54479c80f094
César <cesarlpb@gmail.com>
2024-07-23 16:08:29 +0200

    Añado comentarios en script de bash

--- startup.sh
+++ startup.sh
@@ -4,8 +4,13 @@
   cd <carpeta_de_proyecto>
 
   # NO CAMBIAR:
-  python -m venv .venv && source .venv/bin/activate
+  # Creamos .venv:
+  python -m venv .venv
+  # Activamos .venv:
+  source .venv/bin/activate
+  # Instalo pips:
   pip install -r requirements.txt
+  # Comandos de Django:
   python manage.py collectstatic --noinput # si arroja error, revisar STATIC_ROOT
   python manage.py makemigrations && python manage migrate
   python manage.py runserver
