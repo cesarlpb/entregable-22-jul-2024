055aabeb09eb5a3c83013ab7b072c0574963c7a5
César <cesarlpb@gmail.com>
2024-07-23 19:30:17 +0200

    Ejemplo de envío de email con HTML

--- proyecto20/contact/views.py
+++ proyecto20/contact/views.py
@@ -14,12 +14,19 @@ def contact_view(request):
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
+            
+            subject = f'Mensaje de {name}'
+            html_message = f"<h2>Mensaje:</h2><p>{message}</p>"
+            # Ejemplo de foto:
+            # <div>Foto random:<img src='https://plus.unsplash.com/premium_photo-1661856452176-d919ec23494a?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></div>
+            plain_message = message  # Puedes tener una versión en texto plano del mensaje
             send_mail(
-                f'Mensaje de {name}',
-                message,
+                subject,
+                plain_message,  # Este es el mensaje en texto plano
                 email,
                 [EMAIL_HOST_RECEIVER],  # Cambia esto por el email donde quieres recibir los mensajes
                 fail_silently=False,
+                html_message=html_message  # Este es el mensaje en HTML
             )
             return HttpResponseRedirect(reverse('contact:thanks'))
     else:
