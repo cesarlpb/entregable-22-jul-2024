73c1cbdfab893166d7c5208db1b454a35c009e4e
César <cesarlpb@gmail.com>
2024-07-23 16:13:25 +0200

    mas cambios

--- startup.sh
+++ startup.sh
@@ -1,17 +1,16 @@
-  #!/bin/bash
+#!/bin/bash
 
-  # Cambiar solo carpeta proyecto
-  cd <carpeta_de_proyecto>
+cd <carpeta_de_proyecto>
 
-  # NO CAMBIAR:
-  # Creamos .venv:
-  python -m venv .venv
-  # Activamos .venv:
-  source .venv/bin/activate
-  # Instalo pips:
-  pip install -r requirements.txt
-  # Comandos de Django:
-  python manage.py collectstatic --noinput # si arroja error, revisar STATIC_ROOT
-  python manage.py makemigrations && python manage migrate
-  python manage.py runserver
-  # NO CAMBIAR ^^^
+# NO CAMBIAR:
+# Creamos .venv:
+python -m venv .venv
+# Activamos .venv:
+source .venv/bin/activate
+# Instalo pips:
+pip install -r requirements.txt
+# Comandos de Django:
+python manage.py collectstatic --noinput # si arroja error, revisar STATIC_ROOT
+python manage.py makemigrations && python manage migrate
+python manage.py runserver
+# NO CAMBIAR ^^^
