ba00b72b6bfa1ecce872a6c2159fdf1aa03ae867
César <cesarlpb@gmail.com>
2024-07-23 19:19:21 +0200

    comentario

--- proyecto20/proyecto20/settings.py
+++ proyecto20/proyecto20/settings.py
@@ -138,6 +138,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ENV["EMAIL_HOST_USER"]
+EMAIL_HOST_USER = ENV["EMAIL_HOST_USER"] # Arroja un KeyError si no existe el key en ENV
 EMAIL_HOST_PASSWORD = ENV["APP_PASS"]
 EMAIL_HOST_RECEIVER = ENV["EMAIL_HOST_RECEIVER"]
